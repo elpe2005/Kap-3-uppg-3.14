0b004ee78ab0bf2e5e79fce186b4dd13e5a3c317
Admin <Admin@TEB127025>
2022-09-30 10:59:49 +0200

    Add project files.

---
+++ Kap 3 uppg 3.14/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uppgift313
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Har du gått ut gymnasiumet? svara j för ja och n för nej." );
+            string svar = Console.ReadLine();
+            Console.WriteLine("Hur gammal är du?");
+            int ålder =int.Parse(Console.ReadLine());
+
+           
+
+        }
+    }
+}
